cbb40af24f42ec1441b68138f725d796b1e44601
Pavel Volchenko <2793553@gmail.com>
2024-01-30 07:50:32 +0300

    hw02 done

---
+++ hw02/multiplication_table.py
@@ -0,0 +1,18 @@
+# Процедурная парадигма — возможность повторного переиспользования кода,
+# так как он упакован в процедуры, которые можно сохранить,
+# а потом вызвать откуда угодно
+def print_table(columns):
+    for i in range(1, columns + 1):
+        for j in range(1, 10):
+            print(f"{i} x {j} = {i * j}")
+        print()
+
+
+columns = int(input("Enter the number of columns table: "))
+print_table(columns)
+
+# Структурная парадигма — возможность быстрой проверки кода на работоспособность
+for i in range(1, columns + 1):
+    for j in range(1, 10):
+        print(f"{i} x {j} = {i * j}")
+    print()

587547cba8e54f7dcbc35181e08b276cf7fbd15a
Pavel Volchenko <2793553@gmail.com>
2024-02-05 01:33:24 +0300

    hw06 done

---
+++ hw06/binary_search.py
@@ -0,0 +1,26 @@
+class BinarySearch:
+    def __init__(self, array, number):
+        self.array = array
+        self.number = number
+        self.left = 0
+        self.right = len(array)
+        self.middle = (self.left + self.right) // 2
+
+    def search(self):
+        while self.left <= self.right:
+            self.middle = (self.left + self.right) // 2
+
+            if self.array[self.middle] == self.number:
+                return self.middle
+            elif self.array[self.middle] < self.number:
+                self.left = self.middle + 1
+            else:
+                self.right = self.middle - 1
+
+        return -1
+
+
+arr = [1, 2, 3, 5, 7, 9, 11, 13]
+n = 13
+
+print(BinarySearch(arr, n).search())
